6f0d6b791eafb559c67797575aa23cbf2c9dd3bd
Loren Micheloni <lmicheloni@atlassian.com>
2019-04-29 13:58:34 -0500

    Add producer implementation that sends assets to streaming appliance

--- pkg/producer/http.go
+++ pkg/producer/http.go
@@ -1,1 +1,36 @@
 package producer
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/asecurityteam/nexpose-vuln-notifier/pkg/domain"
+)
+
+// AssetProducer holds streaming appliance configuration
+type AssetProducer struct {
+	HTTPClient *http.Client
+	Endpoint   string
+}
+
+// Produce publishes sends the asset event to the streaming appliance
+func (p *AssetProducer) Produce(ctx context.Context, asset domain.Asset) error {
+	body, _ := json.Marshal(asset)
+	req, err := http.NewRequest(http.MethodPost, p.Endpoint, bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/octet-stream")
+	res, err := p.HTTPClient.Do(req.WithContext(ctx))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected response from streaming appliance: %d", res.StatusCode)
+	}
+	return nil
+}
