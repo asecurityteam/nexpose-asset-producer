f61f2343868ded5250fc0e150891c84e11bdb481
Loren Micheloni <lmicheloni@atlassian.com>
2019-05-14 17:00:09 -0500

    Add tests for AssetPayloadToAssetEvent()

--- pkg/assetfetcher/asset_test.go
+++ pkg/assetfetcher/asset_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/asecurityteam/nexpose-asset-producer/pkg/domain"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -55,4 +56,60 @@ func TestLastAssessedForVulnerabilities(t *testing.T) {
 		})
 	}
 
+}
+
+func TestAssetPayloadToAssetEventSuccess(t *testing.T) {
+	date := time.Date(2019, time.April, 22, 15, 2, 44, 0, time.UTC)
+	asset := Asset{
+		ID:      1,
+		IP:      "127.0.0.1",
+		History: assetHistoryEvents{AssetHistory{Type: "SCAN", Date: "2019-04-22T15:02:44.000Z"}},
+	}
+	expectedAssetEvent := domain.AssetEvent{
+		ID:          1,
+		IP:          "127.0.0.1",
+		LastScanned: date,
+	}
+
+	assetEvent, err := asset.AssetPayloadToAssetEvent()
+	assert.NoError(t, err)
+	assert.Equal(t, expectedAssetEvent, assetEvent)
+}
+
+func TestAssetPayloadToAssetEventError(t *testing.T) {
+	tests := []struct {
+		name  string
+		asset Asset
+	}{
+		{
+			"No ID",
+			Asset{
+				IP:      "127.0.0.1",
+				History: assetHistoryEvents{AssetHistory{Type: "SCAN", Date: "2019-04-22T15:02:44.000Z"}},
+			},
+		},
+		{
+			"No IP",
+			Asset{
+				ID:      1,
+				History: assetHistoryEvents{AssetHistory{Type: "SCAN", Date: "2019-04-22T15:02:44.000Z"}},
+			},
+		},
+		{
+			"No LastScanned",
+			Asset{
+				ID: 1,
+				IP: "127.0.0.1",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(tt *testing.T) {
+			_, err := test.asset.AssetPayloadToAssetEvent()
+			lastScanned, _ := test.asset.History.lastScannedTimestamp()
+			assert.Equal(t, &MissingRequiredFields{test.asset.ID, test.asset.IP, lastScanned}, err)
+
+		})
+	}
 }
