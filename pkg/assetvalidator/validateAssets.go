79c4fa506c53d079f7b2f930592e991b27d16ade
Brandon Chan <bchan@atlassian.com>
2020-01-28 10:05:17 -0600

    fixed linter issues

--- pkg/assetvalidator/validateAssets.go
+++ pkg/assetvalidator/validateAssets.go
@@ -13,6 +13,8 @@ import (
 type NexposeAssetValidator struct {
 }
 
+// ValidateAssets takes in a list of assets from Nexpose, and returns a list of valid Asset events, and a list of errors based
+// on defined validation rules
 func (v *NexposeAssetValidator) ValidateAssets(ctx context.Context, assets []domain.Asset, scanID string) ([]domain.AssetEvent, []error) {
 	assetEventListList := []domain.AssetEvent{}
 	errorList := []error{}
@@ -40,23 +42,23 @@ func (v *NexposeAssetValidator) getScanTime(asset domain.Asset, scanID string) (
 			if strconv.FormatInt(evt.ScanID, 10) == scanID {
 				scanTime, err := time.Parse(time.RFC3339, evt.Date)
 				if err != nil {
-					return time.Time{}, &domain.InvalidScanTime{scanID, scanTime, asset.ID, asset.IP, asset.HostName, err}
+					return time.Time{}, &domain.InvalidScanTime{ScanID: scanID, ScanTime: scanTime, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName, Inner: err}
 				}
 				if scanTime.IsZero() {
-					return time.Time{}, &domain.InvalidScanTime{scanID, scanTime, asset.ID, asset.IP, asset.HostName, errors.New("scan time is zero")}
+					return time.Time{}, &domain.InvalidScanTime{ScanID: scanID, ScanTime: scanTime, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName, Inner: errors.New("scan time is zero")}
 				}
 				return scanTime, nil
 			}
 		}
 	}
-	return time.Time{}, &domain.ScanIDForLastScanNotInAssetHistory{scanID, asset.ID, asset.IP, asset.HostName}
+	return time.Time{}, &domain.ScanIDForLastScanNotInAssetHistory{ScanID: scanID, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName}
 }
 
 // AssetPayloadToAssetEvent translates a Nexpose Asset API response payload
 // into an AssetEvent for downstream services.
 func (v *NexposeAssetValidator) assetPayloadToAssetEvent(asset domain.Asset, scanTime time.Time) (domain.AssetEvent, error) {
 	if asset.ID == 0 || (asset.IP == "" && asset.HostName == "") {
-		return domain.AssetEvent{}, &domain.MissingRequiredInformation{asset.ID, asset.IP, asset.HostName, scanTime}
+		return domain.AssetEvent{}, &domain.MissingRequiredInformation{AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName, AssetScanTime: scanTime}
 	}
 	return domain.AssetEvent{
 		ID:       asset.ID,
