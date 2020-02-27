175244bd94fe58d4efb4098d0edc0095d6ec86d4
iamnotbrandonchan <53226503+iamnotbrandonchan@users.noreply.github.com>
2020-02-27 16:36:48 -0600

    deleted warning log, and custom stats (#41)

--- pkg/handlers/v1/nexposeassetproducer_test.go
+++ pkg/handlers/v1/nexposeassetproducer_test.go
@@ -187,7 +187,6 @@ func TestNexposeAssetProducerHandlerMultipleErrors(t *testing.T) {
 
 	mockAssetFetcher.EXPECT().FetchAssets(gomock.Any(), "12345").Return(assetList, nil)
 	mockAssetValidator.EXPECT().ValidateAssets(gomock.Any(), assetList, scanID).Return(validAssetList, errorList)
-	mockLogger.EXPECT().Warn(gomock.Any()).Times(4)
 
 	handler := NexposeScannedAssetProducer{
 		Producer:       mockProducer,
