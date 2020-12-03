59b7f050c3d415466d755fff962f5c698fdd8dfc
iamnotbrandonchan <53226503+iamnotbrandonchan@users.noreply.github.com>
2020-12-03 11:22:54 -0500

    Revert "Psd 858/add scan type to schema (#46)"
    This reverts commit 05cb6c8b2360e2391538d027f3eb949f2e98c6d6.

--- pkg/assetvalidator/validateAssets.go
+++ pkg/assetvalidator/validateAssets.go
@@ -9,49 +9,22 @@ import (
 	"github.com/asecurityteam/nexpose-asset-producer/pkg/domain"
 )
 
-// NexposeScanType is a type alias to signify type of scan according to Nexpose
-type NexposeScanType string
-
-func (nst NexposeScanType) String() string {
-	return string(nst)
-}
-
-const (
-	automated NexposeScanType = "Automated"
-	manual    NexposeScanType = "Manual"
-	scheduled NexposeScanType = "Scheduled"
-	agent     NexposeScanType = "Agent"
-)
-
-// ScanType is a type alias that generifies a scan to either local or remote
-// The purpose is to not expose Nexpose specifics further down the pipeline
-type ScanType string
-
-func (st ScanType) String() string {
-	return string(st)
-}
-
-const (
-	local  ScanType = "local"
-	remote ScanType = "remote"
-)
-
 // NexposeAssetValidator is used to validate a list of retrieved Assets
 type NexposeAssetValidator struct {
 }
 
 // ValidateAssets takes in a list of assets from Nexpose, and returns a list of valid Asset events, and a list of errors based
 // on defined validation rules
-func (v *NexposeAssetValidator) ValidateAssets(ctx context.Context, assets []domain.Asset, scanInfo domain.ScanInfo) ([]domain.AssetEvent, []error) {
+func (v *NexposeAssetValidator) ValidateAssets(ctx context.Context, assets []domain.Asset, scanID string) ([]domain.AssetEvent, []error) {
 	assetEventListList := []domain.AssetEvent{}
 	errorList := []error{}
 	for _, asset := range assets {
-		scanTime, err := v.getScanTime(asset, scanInfo)
+		scanTime, err := v.getScanTime(asset, scanID)
 		if err != nil {
 			errorList = append(errorList, err)
 			continue
 		}
-		assetEvent, err := v.assetPayloadToAssetEvent(asset, scanTime, scanInfo)
+		assetEvent, err := v.assetPayloadToAssetEvent(asset, scanTime)
 		if err != nil {
 			errorList = append(errorList, err)
 			continue
@@ -62,61 +35,40 @@ func (v *NexposeAssetValidator) ValidateAssets(ctx context.Context, assets []dom
 }
 
 // getScanTime searches through the asset's event history for a scan event with the ScanID
-// that matches the ScanID of the scan completion event that triggered the pipeline, and also returns the type of scan.
-func (v *NexposeAssetValidator) getScanTime(asset domain.Asset, scanInfo domain.ScanInfo) (time.Time, error) {
-	var scanType ScanType
-	nexposeScanType := NexposeScanType(scanInfo.ScanType)
-	if nexposeScanType == agent {
-		scanType = local
-	} else {
-		scanType = remote
-	}
+// that matches the ScanID of the scan completion event that triggered the pipeline.
+func (v *NexposeAssetValidator) getScanTime(asset domain.Asset, scanID string) (time.Time, error) {
 	for _, evt := range asset.History {
 		scanTime, err := time.Parse(time.RFC3339, evt.Date)
-		if evt.Type == "SCAN" && scanType == remote {
-			if strconv.FormatInt(evt.ScanID, 10) == scanInfo.ScanID {
+		switch evt.Type {
+		case "SCAN":
+			if strconv.FormatInt(evt.ScanID, 10) == scanID {
 				if err != nil {
-					return time.Time{}, &domain.InvalidScanTime{ScanID: scanInfo.ScanID, ScanTime: scanTime, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName, Inner: err}
+					return time.Time{}, &domain.InvalidScanTime{ScanID: scanID, ScanTime: scanTime, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName, Inner: err}
 				}
 				if scanTime.IsZero() {
-					return time.Time{}, &domain.InvalidScanTime{ScanID: scanInfo.ScanID, ScanTime: scanTime, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName, Inner: errors.New("scan time is zero")}
+					return time.Time{}, &domain.InvalidScanTime{ScanID: scanID, ScanTime: scanTime, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName, Inner: errors.New("scan time is zero")}
 				}
 				return scanTime, nil
 			}
-		}
-		if evt.Type == "AGENT-IMPORT" && scanType == local {
-			startScanTime, startErr := time.Parse(time.RFC3339, scanInfo.StartTime)
-			endScanTime, endErr := time.Parse(time.RFC3339, scanInfo.EndTime)
-			if err != nil || startErr != nil || endErr != nil {
-				return time.Time{}, &domain.InvalidScanTime{ScanID: scanInfo.ScanID, ScanTime: scanTime, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName, Inner: err}
-			}
-
-			if scanTime.After(startScanTime) && scanTime.Before(endScanTime) {
+		case "AGENT-IMPORT":
+			if time.Since(scanTime).Hours() < 24 {
 				return scanTime, nil
 			}
 		}
 	}
-	return time.Time{}, &domain.ScanIDForLastScanNotInAssetHistory{ScanID: scanInfo.ScanID, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName}
+	return time.Time{}, &domain.ScanIDForLastScanNotInAssetHistory{ScanID: scanID, AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName}
 }
 
 // assetPayloadToAssetEvent translates a Nexpose Asset API response payload
 // into an AssetEvent for downstream services.
-func (v *NexposeAssetValidator) assetPayloadToAssetEvent(asset domain.Asset, scanTime time.Time, scanInfo domain.ScanInfo) (domain.AssetEvent, error) {
+func (v *NexposeAssetValidator) assetPayloadToAssetEvent(asset domain.Asset, scanTime time.Time) (domain.AssetEvent, error) {
 	if asset.ID == 0 || (asset.IP == "" && asset.HostName == "") {
 		return domain.AssetEvent{}, &domain.MissingRequiredInformation{AssetID: asset.ID, AssetIP: asset.IP, AssetHostname: asset.HostName}
-	}
-	var scanType ScanType
-	nexposeScanType := NexposeScanType(scanInfo.ScanType)
-	if nexposeScanType == agent {
-		scanType = local
-	} else {
-		scanType = remote
 	}
 	return domain.AssetEvent{
 		ID:       asset.ID,
 		Hostname: asset.HostName,
 		IP:       asset.IP,
 		ScanTime: scanTime,
-		ScanType: string(scanType),
 	}, nil
 }
--- pkg/domain/assetValidator.go
+++ pkg/domain/assetValidator.go
@@ -7,5 +7,5 @@ import (
 // AssetValidator represents the interface you can use to validate all Assets. It returns
 // a list of valid assets and a list of errors that reflect invalid assets
 type AssetValidator interface {
-	ValidateAssets(ctx context.Context, assets []Asset, scanInfo ScanInfo) ([]AssetEvent, []error)
+	ValidateAssets(ctx context.Context, assets []Asset, scanID string) ([]AssetEvent, []error)
 }
